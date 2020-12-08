37219715c449d388985451df57d71d944c9bbb9c
carrliitos <salazar.ten80@gmail.com>
2020-12-08 00:12:43 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-var mysql = require('mysql')
-
-var con = mysql.createConnection({
-	host: "washington.uww.edu",
-	user: "salazarbc24",
-	password: "bs0288"
-});
-
-// con.connect(function(err) {
-// 	if(err) throw err;
-// 	console.log("Connected to database!");
-// });
-
-/**
-* When the user clicks on the button,
-* toggle between hiding and showing the dropdown content
-*/
 function dropdown() {
 	document.getElementById("myDropdown").classList.toggle("show");
+	console.log("Hello!");
 }
 
 // Close the dropdown menu if the user clicks outside of it
